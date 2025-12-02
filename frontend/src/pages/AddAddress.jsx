d23bf918cb5201f51e4276439260fe9a714f166d
DukeMochama2022 <dukemochama21@gmail.com>
2025-12-02 19:43:27 +0300

    Added some code

--- frontend/src/pages/AddAddress.jsx
+++ frontend/src/pages/AddAddress.jsx
@@ -1,9 +1,118 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const AddAddress = () => {
+  const { navigate } = useContext(AppContext);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    city: "",
+    country: "",
+    zipCode: "",
+    state: "",
+  });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    toast.success("Address added successifully !");
+    navigate("/checkout")
+  };
   return (
-    <div>AddAddress</div>
-  )
-}
+    <div
+      className="py-12 bg-[#0B482F]"
+      style={{ backgroundImage: `url(${assets.footer_img})` }}
+    >
+      <div>
+        <form
+          onSubmit={submitHandler}
+          className="max-w-xl w-full mx-auto text-white p-4 border border-white rounded"
+        >
+          <div className="text-white flex flex-col gap-2 mb-4">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              required
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full border border-white outline-none py-3 rounded p-2"
+            />
+          </div>
 
-export default AddAddress
+          <div className="text-white flex flex-col gap-2 mb-4">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              required
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full border border-white outline-none py-3 rounded p-2"
+            />
+          </div>
+
+          <div className="text-white flex flex-col gap-2 mb-4">
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              placeholder="Enter your city"
+              required
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              className="w-full border border-white outline-none py-3 rounded p-2"
+            />
+          </div>
+          <div className="text-white flex flex-col gap-2 mb-4">
+            <label htmlFor="country">Country</label>
+            <input
+              type="text"
+              placeholder="Enter your country"
+              required
+              name="country"
+              value={formData.country}
+              onChange={handleChange}
+              className="w-full border border-white outline-none py-3 rounded p-2"
+            />
+          </div>
+          <div className="text-white flex flex-col gap-2 mb-4">
+            <label htmlFor="zipCode">Zip code</label>
+            <input
+              type="number"
+              placeholder="Enter your country"
+              required
+              name="zipCode"
+              value={formData.zipCode}
+              onChange={handleChange}
+              className="w-full border border-white outline-none py-3 rounded p-2"
+            />
+          </div>
+          <div className="text-white flex flex-col gap-2 mb-4">
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              placeholder="Enter your state"
+              required
+              name="state"
+              value={formData.state}
+              onChange={handleChange}
+              className="w-full border border-white outline-none py-3 rounded p-2"
+            />
+          </div>
+          <button className="bg-primary w-full rounded-md text-white cursor-pointer px-6 py-2">Add Address</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddAddress;
--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const Checkout = () => {
+  const { cart, navigate, getCartTotal, currency } = useContext(AppContext);
+  const [address, setAddress] = useState("USA");
+  const [paymentMethod, setPaymentMethod] = useState("cod");
   return (
-    <div>Checkout</div>
-  )
-}
+    <div
+      className="py-12 bg-[#0B482F]"
+      style={{ backgroundImage: `url(${assets.footer_img})` }}
+    >
+      <h1 className="font-bold text-white text-center text-3xl">Checkout</h1>
+      <div className="flex flex-col md:flex-row items-center justify-evenly gap-6">
+        <div>
+          <h1 className=" my-5 text-3xl font-bold text-white text-center">
+            Cart Summary
+          </h1>
+          <div className=" my-5 max-w-4xl w-full p-4 rounded-md border bg-white border-primary mb-8">
+            {cart.map((item) => (
+              <div className="w-full flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                  <img src={item.images[0]} className="w-20 h-20" alt="" />
+                  <p>{item.name}</p>
+                </div>
+                <p>
+                  {currency}
+                  {item.offerPrice}
+                </p>
+              </div>
+            ))}
+            <p>
+              Total:{currency}
+              {getCartTotal()}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6 text-white">
+          <h1 className="text-3xl font-bold">Order Summary</h1>
+          <div className="flex flex-col gap-6">
+            <label htmlFor="address">Select Address</label>
+            <select
+              className="w-full outline-none border border-primary p-2"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            >
+              <option className="text-gray-800">USA</option>
+              <option className="text-gray-800">INDIA</option>
+            </select>
+            <button
+              onClick={() => navigate("/add-address")}
+              className="bg-primary rounded-md text-white cursor-pointer px-6 py-2"
+            >
+              Add New Address
+            </button>
+          </div>
 
-export default Checkout
+          <div className="flex flex-col gap-6">
+            <label htmlFor="address">Select Payment Method</label>
+            <select
+              className="w-full outline-none border border-primary p-2"
+              value={paymentMethod}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            >
+              <option value={"cod"} className="text-gray-800">
+                COD
+              </option>
+              <option value={"online"} className="text-gray-800">
+                ONLINE
+              </option>
+            </select>
+          </div>
+          <button className="bg-primary rounded-md text-white cursor-pointer px-6 py-2">
+            {paymentMethod === "cod" ? "Place Order" : "Pay Now"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
